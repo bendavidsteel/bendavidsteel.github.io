91edbd42b646a713640aabd625ca4152ac4f518e
Ben Steel <bendavidsteel@gmail.com>
2021-12-24 18:21:52 +0000

    North bells page now in good place

--- src/views/misc/NorthBells.vue
+++ src/views/misc/NorthBells.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="content">
         <div class="flex-box-page">
-            <img :src="require('@/assets/northbells.jpg')" alt="Picture of the Northumbrian Bells performing" style="width:90%;height:90%;">
+            <img :src="require('@/assets/northbells.jpg')" alt="Picture of the Northumbrian Bells performing" style="width:50%;height:90%;">
             <div class="flex-box-col">
                 <div class="text-box">
                     <h1 id='name'>
@@ -9,16 +9,24 @@
                     </h1>
                     <h4 id='descrip'>
                         Harp and Violin duo based in Northumberland <br>
-                        Available to play at events
+                        Playing a variety of English and Scottish traditional folk music <br>
+                        Available to play at weddings and events
                     </h4>
                 </div>
-                <img :src="require('@/assets/heather.jpg')" alt="Picture of the some bell heather" style="width:100%;height:36%;">
+                <img :src="require('@/assets/heather.jpg')" alt="Picture of the some bell heather" style="width:100%;height:40%;">
             </div>
         </div>
     </section>
     <footer class="footer">
-        <div class="tabs">
-            <a class="tab" href="https://encoremusicians.com/Northumbrian-Bells" target="_blank">Encore</a>
+        <div class="footer-gap"></div>
+        <div class="main-footer">
+            <div class="tabs">
+                <div class="tab" >
+                    <a class="link" href="https://encoremusicians.com/Northumbrian-Bells" target="_blank">
+                        <b>Encore</b>
+                    </a>
+                </div>
+            </div>
         </div>
     </footer>
 </template>
@@ -27,7 +35,7 @@
 .content {
     height: 100vh;
     padding: 0px;
-    background-color: white;
+    background-color: lightpink;
 }
 
 .flex-box-page {
@@ -42,6 +50,8 @@
     display: flex;
     flex-direction: column;
     justify-content: space-between;
+    height: 90%;
+    width: 50%;
 }
 
 .link {
@@ -49,41 +59,61 @@
 }
 
 .text-box {
+    padding-top: 80px;
     padding-left: 40px;
 }
 
 .footer {
     height: 10%;
     width: 100%;
-    background-color: fuchsia;
     position: fixed;
     bottom: 0;
 }
 
+.footer-gap {
+    background-color: silver;
+    height: 10%;
+}
+
+.main-footer {
+    background-color: skyblue;
+    height: 90%;
+}
+
 #name {
-    font-family: Georgia, Verdana, sans-serif;
-    font-size: 54px;
-    color: black;
+    font-family: Garamond, "Times New Roman", serif;
+    font-size: 90px;
+    color: darkorchid;
+    text-shadow: -2px -2px 0 silver, 2px -2px 0 silver, -2px 2px 0 silver, 2px 2px 0 silver;
 }
 
 #descrip {
     font-family: Georgia, Verdana, sans-serif;
-    font-size: 26px;
-    color:black;
+    font-size: 20px;
+    color:darkslategray;
 }
 
 .tabs {
-  padding-top: 20px;
+  padding-top: 10px;
+  padding-bottom: 20px;
   display: flex;
   flex-direction: row;
   justify-content: flex-end;
 }
 
 .tab {
+  margin-right: 80px;
+  padding: 10px;
+}
+
+.tab:hover {
+    background-color: rgba(1, 1, 1, 0.1);
+}
+
+.link {
   font-size: 40px;
   font-family: Arial, Helvetica, sans-serif;
   text-decoration: none;
-  color: black;
-  padding-right: 80px;
+  color: darkslategray;
 }
 </style>
